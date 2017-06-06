1be54a004b4671c11ab94cf1ca32fcac6f8e349a
dingxue <304875853@qq.com>
2017-06-06 13:40:52 +0800

    ui state

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,28 @@
-import React from 'react'// eslint-disable-line
+import React , { Component }from 'react'// eslint-disable-line
+import ui from 'redux-ui'
 
-const Login = () => (
-  <div>
-    <h2>login by ,laotie hehe </h2>
-  </div>
-)
+class Login extends Component {
+  render () {
+    const { loading } = this.props.ui
+    return (
+      <div>
+        <h2>login by ,laotie hehe{loading}</h2>
+        <button onClick={this.clickTest.bind(this)}>reset</button>
+      </div>
+    )
+  }
+  clickTest() {
+    this.props.updateUI({
+      loading: false
+    })
+  }
+}
 
-export default Login
+const loginContainer = ui({
+  key: 'logins',
+  state: {
+    loading: true
+  }
+})(Login)
+
+export default loginContainer
